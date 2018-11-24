c6099953235a7a3f5b4c27ddd1e77cd99fb42b70
davidrileyschultz <rileyschultz@gmail.com>
2018-11-24 13:07:22 -0800

    added in picture upload component to allow users to find a local file to use

---
+++ src/components/_journal/PictureUpload.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class PictureUpload extends Component {
+    render() {
+        return (<div className="picture-upload">
+            <p> Add an Image: </p>
+            <input type="file" onChange={this.uploadPhoto} />
+            <p>
+                <button>Finish</button>
+            </p>
+        </div>
+        )
+    }
+}
+
+export default PictureUpload;
