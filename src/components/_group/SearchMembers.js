7d3786866cb7257154fa574fc961f3e120bb1421
davidrileyschultz <rileyschultz@gmail.com>
2018-11-27 12:02:36 -0800

    added in component to help search for users to add to your group

---
+++ src/components/_group/SearchMembers.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { FormGroup, FormControl } from 'react-bootstrap';
+
+class SearchMembers extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    onInputChange(term) {
+        this.setState({ term: term });
+        this.props.onSearchEnter(this.state.term);
+    }
+
+    render() {
+        return (
+            <FormGroup>
+                <FormControl
+                    placeholder="Search for your friends"
+                    type="text"
+                    name="term"
+                    value={this.state.term}
+                    onChange={(event) => this.onInputChange(event.target.value)}
+                />
+            </FormGroup>
+        );
+    }
+}
