4ab03329b980c7fc802572bac752f6e7dcaf962d
davidrileyschultz <rileyschultz@gmail.com>
2018-11-27 12:13:29 -0800

    can now view members as list.

---
+++ src/components/_group/MemberListItem.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { ListGroupItem } from 'react-bootstrap';
+
+class MemberListItem extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    isActive() {
+        const existingMembers = this.props.existingMembers.amp(a => a.value);
+        const thisMember = this.props.value;
+        if(existingMembers.include(thisMember)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    render() {
+        const memberToAdd = { key: this.props.value, value: this.props.value, display: this.props.display };
+        return (
+            <ListGroupItem active={this.isActive()} onClick={() => this.props.onMemberSelect(memberToAdd)}>
+                {this.props.display}
+            </ListGroupItem>
+        );
+    }
+}
+
+export default MemberListItem;
